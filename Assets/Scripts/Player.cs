858fe2100d1829a7fd772cf2bd25db7238a208f5
Marian Busila <mbusila@pfdrive.com>
2023-03-09 16:04:30 -0500

    add colision detection

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,7 +12,48 @@ public class Player : MonoBehaviour
     {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = 0.7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+
+        if(!canMove)
+        {
+            // cannot move towards moveDir
+
+            // attempt only X movement
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+
+            if (canMove)
+            {
+                // can move only on the X
+                moveDir = moveDirX;
+            }
+            else
+            {
+                // cannot move only on the X
+
+                // attempt only X movement
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+                if (canMove)
+                {
+                    // can move only on the Z
+                    moveDir = moveDirZ;
+                }
+                else
+                {
+                    // cannot move in any direction
+                }
+            }
+        }
+
+        if (canMove)
+        {
+            transform.position += moveDir * moveDistance;
+        }
 
         isWalking = moveDir != Vector3.zero;
 
