2b43566ff1a2cad6d35942e417e774ef4c76e0b9
Marian Busila <mbusila@pfdrive.com>
2023-03-12 14:00:51 -0400

    set kitchen object parent

--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -6,8 +6,35 @@ public class KitchenObject : MonoBehaviour
 {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
+    private ClearCounter clearCounter;
+
     public KitchenObjectSO GetKitchenObjectSO()
     {
         return kitchenObjectSO;
+    }
+
+    public void SetClearCounter(ClearCounter clearCounter)
+    {
+        // change the clear counter parent
+        if(this.clearCounter != null)
+        {
+            this.clearCounter.ClearKitchenObject();
+        }
+        this.clearCounter = clearCounter;
+        if (clearCounter.HasKitchenObject())
+        {
+            Debug.Log("Counter already has a kitchen object");
+        }
+
+        clearCounter.SetKitchenObject(this);
+
+        // change the visual to belong to the other parent
+        transform.parent = clearCounter.GetKitchenObjectFollowTransform();
+        transform.localPosition = Vector3.zero;
+    }
+
+    public ClearCounter GetClearCounter()
+    {
+        return clearCounter;
     }
 }
