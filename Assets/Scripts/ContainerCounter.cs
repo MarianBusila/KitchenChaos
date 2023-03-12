74bf9b45d7f680a48ceac229d038422cae567a6e
Marian Busila <mbusila@pfdrive.com>
2023-03-12 16:07:09 -0400

    add container counter and refactor with prefab variants and base counters

---
+++ Assets/Scripts/ContainerCounter.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System;
+using UnityEngine;
+
+public class ContainerCounter : BaseCounter
+{
+    public event EventHandler OnPlayerGrabbedObject;
+
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    public override void Interact(Player player)
+    {
+        if (!HasKitchenObject())
+        {
+            // create the kitchen object
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
+            OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
