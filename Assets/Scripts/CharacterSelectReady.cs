c53da4471c69a52679349fd19b89ce4c5b7ed52c
Marian Busila <mbusila@pfdrive.com>
2023-05-16 16:50:42 -0400

    remove lobby when not needed

--- Assets/Scripts/CharacterSelectReady.cs
+++ Assets/Scripts/CharacterSelectReady.cs
@@ -43,6 +43,7 @@ public class CharacterSelectReady : NetworkBehaviour
         if (allClientsReady)
         {
             Debug.Log("All players ready");
+            KitchenGameLobby.Instance.DeleteLobby();
             Loader.LoadNetwork(Loader.Scene.GameScene);
         }
     }
